03f114843d95d7d939ff5069ab453de5282d5b5d
Tyler Hilliard <tylerghilliard@gmail.com>
2020-06-11 18:44:06 -0500

    Added event integration

--- scripts/itineraries/itinerariesList.js
+++ scripts/itineraries/itinerariesList.js
@@ -1,5 +1,13 @@
+const eventList = (eventsObject) => {
+    const eventsHTML= eventsConverter(eventsObject)
+    const eventsDivElement= document.querySelector(".event__details")
+    eventsDivElement.innerHTML += eventsHTML
+}
+
+
+
 const itinerariesList = (itineraries) =>{
-    //for (const currentitinerariesobject of itineraries){
+    
         const itinerariesHTML= itinerariesConverter(itineraries)
         const itinerariesdivelement= document.querySelector(".aside__saved__itineraries")
         itinerariesdivelement.innerHTML += itinerariesHTML
@@ -7,17 +15,26 @@ const itinerariesList = (itineraries) =>{
         let eaterycounter = -1
 
         for (const attraction of itineraries.attractions){
+            console.log(itineraries.attractions)
             attractioncounter++
             const itinerariesAttractionsHTML= itinerariesAttractionsConverter(itineraries.attractions,attractioncounter)
-            const itinerariesattractionsdivelement= document.querySelector(".itinerary__attractions")
+            console.log(itineraries.parks.name)
+            const itinerariesattractionsdivelement= document.querySelector(`.itinerary__attractions__${itineraries.parks.id}`)
             itinerariesattractionsdivelement.innerHTML += itinerariesAttractionsHTML
         }
 
         for (const eatery of itineraries.eateries){
+            console.log(itineraries.eateries)
             eaterycounter++
             const itinerariesEateriesHTML= itinerariesEateriesConverter(itineraries.eateries,eaterycounter)
-            const itinerarieseateriesdivelement= document.querySelector(".itinerary__eateries")
+            const itinerarieseateriesdivelement= document.querySelector(`.itinerary__eateries__${itineraries.parks.id}`)
             itinerarieseateriesdivelement.innerHTML += itinerariesEateriesHTML
         }
 
-    } 
+        const eventDetailVisibilityButtons = document.querySelectorAll(`.button__events__${itineraries.parks.parkCode}`)
+        eventDetailVisibilityButtons.forEach(button => {
+            button.addEventListener("click", clickEvent => {
+                document.getElementById(`details__${clickEvent.target.id}`).classList.toggle("hidden__details")
+            })
+        })
+    }
--- scripts/itineraries/itineries.js
+++ scripts/itineraries/itineries.js
@@ -1,5 +1,6 @@
 
 let postObject = {}
+let eventCompare = ""
 const deleteItenerary = () => {
 return fetch(`http://localhost:8088/itineraries`).then(
     (response) => response.json())
@@ -26,14 +27,32 @@ const API = {
        
     }
 }
+const eventCreator = (compare) =>{
+    return fetch("http://localhost:8088/itineraries").then(response => response.json()).then((response) => {
+                    for (const place of response) {
+                        if (compare === place.parks.parkCode) {
+                            return fetch(`https://developer.nps.gov/api/v1/events?&api_key=${keys.npsKey}`)
+                            .then(response => response.json())
+                            .then((event) => {
+                                console.log(event)
+                                for (let number = 0; number < 3; number ++) {
+                                eventList(event.data[number])
+                                }
+                            })
+                    }
+                }
+            })
+}
 
 saveButton.addEventListener("click", (clickEvent) => {
     postObject.parks = natPark;
     postObject.weather = weatherArray
     postObject.attractions = attractionSelectionArray
     postObject.eateries = eaterySelectedCollection
+    eventCompare = postObject.parks.parkCode
     API.saveItineraryEntry(postObject)
     itinerariesList(postObject)
+    eventCreator(eventCompare)
     const parkDivElement = document.querySelector(".preview__parks")
     parkDivElement.innerHTML = ""
     clearAttractionsList()
