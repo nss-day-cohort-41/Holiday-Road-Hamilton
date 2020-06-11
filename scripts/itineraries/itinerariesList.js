37f69f4741a4355a3ac08281fdb83dacf5bbdedb
BradCowart <Brad@TierStrategies.com>
2020-06-11 13:31:26 -0500

    Started Itin List

---
+++ scripts/itineraries/itinerariesList.js
@@ -0,0 +1,8 @@
+const itinerariesList = (itineraries) =>{
+    for (const currentitinerariesobject of itineraries){
+        const itinerariesHTML= itinerariesConverter(currentitinerariesobject)
+        const itinerariesdivelement= document.querySelector(".itinerariescontainer")
+        itinerariesdivelement.innerHTML += itinerariesHTML
+    }
+
+}
