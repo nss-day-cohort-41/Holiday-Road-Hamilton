94043a1d271fc4ae5c40e4d63df3c85af98f4df2
davidbrucetn <severian37@gmail.com>
2020-06-11 08:11:01 -0500

    Added addtl weather funct, reset days on each weather call, cleared innerHTML

--- scripts/weather/weatherList.js
+++ scripts/weather/weatherList.js
@@ -1,10 +1,14 @@
  const weatherList = (weather) =>{
     let dayCnt = 0;
+    const weatherdivelement= document.querySelector(".weathercontainer")
+    weatherdivelement.innerHTML = "";
     for (const currentweatherobject of weather.daily){
         dayCnt++;
-        const weatherHTML= weatherConverter(currentweatherobject,dayCnt)
-        const weatherdivelement= document.querySelector(".weathercontainer")
-        weatherdivelement.innerHTML += weatherHTML
+        if (dayCnt < 6) {
+            const weatherHTML= weatherConverter(currentweatherobject,dayCnt)
+            weatherdivelement.innerHTML += weatherHTML
+            drawWeather(currentweatherobject,dayCnt)
+        }
     }
 
 }
