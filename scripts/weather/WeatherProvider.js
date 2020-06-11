2d2948e880d4c68792a00f9b6222f199e331279e
Tyler Hilliard <tylerghilliard@gmail.com>
2020-06-11 09:55:20 -0500

    key fixes

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,10 +1,10 @@
-//api key c8fb6767591a6ee3fec1033c2dd048dc
+
 let weather=[]
 
 const eventHub = document.querySelector(".weathercontainer")
 
 const getWeather = (lat,long) => { 
-    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,current,minutely&units=imperial&appid=${weatherKey}`)
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,current,minutely&units=imperial&appid=${keys.weatherKey}`)
     .then((response) => response.json())
       .then((parsedWeather) => {
         weather = parsedWeather
